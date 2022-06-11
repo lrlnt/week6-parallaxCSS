0b42afa49f8a77b8f3a3f071f7484d2d3962483c
lrlnt <github@lunt.eu>
2022-06-11 13:31:57 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,4 @@
 console.log("Hello! I'm your javascript!");
 // .getTime on unix timestamp millisekundites
 document.getElementById("year").innerHTML=new Date().getFullYear();
+console.log(document.querySelector("#year").innerHTML);
